ec1639b7084c0a78a438c53d955b23e4bf34cc15
Yura192c <fpm.mironchiYU@bsu.by>
2023-07-01 18:24:46 +0300

    add celery for async sending newsletter

---
+++ src/newsletter/tasks.py
@@ -0,0 +1,50 @@
+from celery import shared_task
+from src.newsletter.models import Dispatch, Client, Message
+from config.settings.development import API_SENDING_URL, API_JWT_TOKEN
+import requests
+
+
+@shared_task
+def send_message_to_client(dispatch_id, client_id):
+    dispatch = Dispatch.objects.get(id=dispatch_id)
+    client = Client.objects.get(id=client_id)
+
+    url = f"{API_SENDING_URL}{client.id}"
+    headers = {"Authorization": f"{API_JWT_TOKEN}"}
+    data = {
+        "id": client.id,
+        "phone": client.phone_number,
+        "text": dispatch.text_message,
+    }
+    # print(f'{url} \n {headers} \n {data}')
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        print(response.status_code)
+        if response.status_code == 200:
+            message = Message.objects.create(
+                status='S',
+                dispatch=dispatch,
+                client=client
+            )
+
+        else:
+            message = Message.objects.create(
+                status='F',
+                dispatch=dispatch,
+                client=client
+            )
+    except requests.exceptions.RequestException:
+        message = Message.objects.create(
+            status='F',
+            dispatch=dispatch,
+            client=client
+        )
+    except Exception as e:
+        message = Message.objects.create(
+            status='F',
+            dispatch=dispatch,
+            client=client
+        )
+        # Обработка ошибки при выполнении запроса
+        print(f"An error occurred: {str(e)}")
