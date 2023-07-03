e1c66cc3be966a22b7f2b804e6c6a69abccb49fe
Yura192c <fpm.mironchiYU@bsu.by>
2023-07-03 18:46:24 +0300

    added a check that only the administrator can see the statistics

--- src/stats/views.py
+++ src/stats/views.py
@@ -24,75 +24,83 @@ def calculate_clients_count(monday):
 
 @login_required
 def statistics(request):
-    current_date = timezone.now().date()
+    if request.user.is_superuser:
+        current_date = timezone.now().date()
 
-    current_week_day = current_date.weekday()
-    # Вычисляем начало и конец недели
-    start_of_week = current_date - timedelta(days=current_date.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
-    dispatches_this_week = Dispatch.objects.filter(start_time__range=(start_of_week, end_of_week))
-    dispatches_this_week_count = dispatches_this_week.count()
+        # error_messages = messages.get_messages(request)
+        # if error_messages:
+        #     error_message = next(error_messages)
 
-    monday = dispatches_this_week.filter(start_time__week_day=2)
-    monday_dispatches_count = monday.count()
-    monday_clients_count = calculate_clients_count(monday)
+        current_week_day = current_date.weekday()
+        # Вычисляем начало и конец недели
+        start_of_week = current_date - timedelta(days=current_date.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
+        dispatches_this_week = Dispatch.objects.filter(start_time__range=(start_of_week, end_of_week))
+        dispatches_this_week_count = dispatches_this_week.count()
 
-    tuesday = dispatches_this_week.filter(start_time__week_day=3)
-    tuesday_dispatches_count = tuesday.count()
-    tuesday_clients_count = calculate_clients_count(tuesday)
+        monday = dispatches_this_week.filter(start_time__week_day=2)
+        monday_dispatches_count = monday.count()
+        monday_clients_count = calculate_clients_count(monday)
 
-    wednesday = dispatches_this_week.filter(start_time__week_day=4)
-    wednesday_dispatches_count = wednesday.count()
-    wednesday_clients_count = calculate_clients_count(wednesday)
+        tuesday = dispatches_this_week.filter(start_time__week_day=3)
+        tuesday_dispatches_count = tuesday.count()
+        tuesday_clients_count = calculate_clients_count(tuesday)
 
-    thursday = dispatches_this_week.filter(start_time__week_day=5)
-    thursday_dispatches_count = thursday.count()
-    thursday_clients_count = calculate_clients_count(thursday)
+        wednesday = dispatches_this_week.filter(start_time__week_day=4)
+        wednesday_dispatches_count = wednesday.count()
+        wednesday_clients_count = calculate_clients_count(wednesday)
 
-    friday = dispatches_this_week.filter(start_time__week_day=6)
-    friday_dispatches_count = friday.count()
-    friday_clients_count = calculate_clients_count(friday)
+        thursday = dispatches_this_week.filter(start_time__week_day=5)
+        thursday_dispatches_count = thursday.count()
+        thursday_clients_count = calculate_clients_count(thursday)
 
-    saturday = dispatches_this_week.filter(start_time__week_day=7)
-    saturday_dispatches_count = saturday.count()
-    saturday_clients_count = calculate_clients_count(saturday)
+        friday = dispatches_this_week.filter(start_time__week_day=6)
+        friday_dispatches_count = friday.count()
+        friday_clients_count = calculate_clients_count(friday)
 
-    sunday = dispatches_this_week.filter(start_time__week_day=1)
-    sunday_dispatches_count = sunday.count()
-    sunday_clients_count = calculate_clients_count(sunday)
+        saturday = dispatches_this_week.filter(start_time__week_day=7)
+        saturday_dispatches_count = saturday.count()
+        saturday_clients_count = calculate_clients_count(saturday)
 
-    # messages = Message.objects.filter()
-    messages = Message.objects.filter(dispatch__in=dispatches_this_week)
-    total_messages_count = messages.count()
-    failed_messages = messages.filter(status='F')
-    failed_messages_count = failed_messages.count()
-    sent_messages = messages.filter(status='S')
-    sent_messages_count = sent_messages.count()
-    pending_messages = messages.filter(status='P')
-    pending_messages_count = pending_messages.count()
+        sunday = dispatches_this_week.filter(start_time__week_day=1)
+        sunday_dispatches_count = sunday.count()
+        sunday_clients_count = calculate_clients_count(sunday)
 
-    context = {
-        'monday_dispatches_count': monday_dispatches_count,
-        'monday_clients_count': monday_clients_count,
-        'tuesday_dispatches_count': tuesday_dispatches_count,
-        'tuesday_clients_count': tuesday_clients_count,
-        'wednesday_dispatches_count': wednesday_dispatches_count,
-        'wednesday_clients_count': wednesday_clients_count,
-        'thursday_dispatches_count': thursday_dispatches_count,
-        'thursday_clients_count': thursday_clients_count,
-        'friday_dispatches_count': friday_dispatches_count,
-        'friday_clients_count': friday_clients_count,
-        'saturday_dispatches_count': saturday_dispatches_count,
-        'saturday_clients_count': saturday_clients_count,
-        'sunday_dispatches_count': sunday_dispatches_count,
-        'sunday_clients_count': sunday_clients_count,
+        # messages = Message.objects.filter()
+        messages = Message.objects.filter(dispatch__in=dispatches_this_week)
+        total_messages_count = messages.count()
+        failed_messages = messages.filter(status='F')
+        failed_messages_count = failed_messages.count()
+        sent_messages = messages.filter(status='S')
+        sent_messages_count = sent_messages.count()
+        pending_messages = messages.filter(status='P')
+        pending_messages_count = pending_messages.count()
 
-        'failed_messages_count': failed_messages_count,
-        'sent_messages_count': sent_messages_count,
-        'pending_messages_count': pending_messages_count,
-        'total_messages_count': total_messages_count,
+        context = {
+            'monday_dispatches_count': monday_dispatches_count,
+            'monday_clients_count': monday_clients_count,
+            'tuesday_dispatches_count': tuesday_dispatches_count,
+            'tuesday_clients_count': tuesday_clients_count,
+            'wednesday_dispatches_count': wednesday_dispatches_count,
+            'wednesday_clients_count': wednesday_clients_count,
+            'thursday_dispatches_count': thursday_dispatches_count,
+            'thursday_clients_count': thursday_clients_count,
+            'friday_dispatches_count': friday_dispatches_count,
+            'friday_clients_count': friday_clients_count,
+            'saturday_dispatches_count': saturday_dispatches_count,
+            'saturday_clients_count': saturday_clients_count,
+            'sunday_dispatches_count': sunday_dispatches_count,
+            'sunday_clients_count': sunday_clients_count,
 
-        'dispatches_this_week': dispatches_this_week,
-        'dispatches_this_week_count': dispatches_this_week_count,
-    }
-    return render(request, 'stats/statistics.html', context)
+            'failed_messages_count': failed_messages_count,
+            'sent_messages_count': sent_messages_count,
+            'pending_messages_count': pending_messages_count,
+            'total_messages_count': total_messages_count,
+
+            'dispatches_this_week': dispatches_this_week,
+            'dispatches_this_week_count': dispatches_this_week_count,
+        }
+        return render(request, 'stats/statistics.html', context)
+    else:
+
+        return render(request, 'stats/no_admin.html')
