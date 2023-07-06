b25f725bd523a0285a52176a968c2118d5f96482
Yura192c <fpm.mironchiYU@bsu.by>
2023-07-06 12:30:43 +0300

    change result_backend to redis

--- config/celery.py
+++ config/celery.py
@@ -1,13 +1,17 @@
 import os
 from celery import Celery
+
 # import django_celery_results
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 app = Celery('config',
-             # broker='amqp://rabbitmq:rabbitmq@test:5672',
+             broker='amqp://rabbitmq:rabbitmq@test:5672',
+             backend='redis://redis:redis@redis:6379/0',
              )
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
 # app.conf.result_backend = 'django-db'
 
 
