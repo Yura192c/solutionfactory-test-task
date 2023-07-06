90d5930e5eae6065dfac5025d5624f89fb7ac485
Yura192c <fpm.mironchiYU@bsu.by>
2023-07-06 03:00:07 +0300

    edit swagger url to docs/

--- routers.py
+++ routers.py
@@ -21,7 +21,7 @@ urlpatterns = [
 
     # YASG documentation
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     # API v1
